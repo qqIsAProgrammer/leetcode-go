00eaa05d04fb5dbd661fbee95ad4220c35247615
qiuyiliang <1162922438@qq.com>
2021-07-31 16:43:02 +0800

    7.31 binary tree

---
+++ binary_tree/no235.go
@@ -0,0 +1,11 @@
+package binary_tree
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+	if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	return root
+}
